42ae577d4e667f6f5dc2c51d458ea775a537ee36
Antonin-Morcrette <81378148+Antonin-Morcrette@users.noreply.github.com>
2022-03-29 14:32:34 +0200

    Add files via upload

--- arduino/Robot_Accueil_ESIEA.cpp
+++ arduino/Robot_Accueil_ESIEA.cpp
@@ -1,172 +1,83 @@
 #include "Robot_Accueil_ESIEA.h"
 
+Stepper TeteStepper(stepsPerRevolution, 10, 11);     // on setup le moteur et son pinout
+Stepper BrasDroitStepper(stepsPerRevolution, 8, 9);  // on setup le moteur et son pinout
+Stepper BrasGaucheStepper(stepsPerRevolution, 6, 7); // on setup le moteur et son pinout*/
 
-//MatriceGauche = Adafruit_BicolorMatrix();
-//MatriceDroite = Adafruit_BicolorMatrix();
+const int stepsPerRevolution = 6400;
+const int MicroGauche = 1; // on setup le pinout des microrupteurs
+const int MicroDroit = 2;  // on setup le pinout des microrupteurs
+const int MicroTete = 3;   // on setup le pinout des microrupteurs
 
-Stepper TeteThetaStepper(stepsPerRevolution, 10, 11);   //on setup le moteur et son pinout
-Stepper TetePhiStepper(stepsPerRevolution, 12, 13);     //on setup le moteur et son pinout
-Stepper BrasDroitStepper(stepsPerRevolution, 8, 9);   //on setup le moteur et son pinout
-Stepper BrasGaucheStepper(stepsPerRevolution, 6, 7);  //on setup le moteur et son pinout*/
-const int MicroGauche = 1;    //on setup le pinout des microrupteurs
-const int MicroDroit = 2;     //on setup le pinout des microrupteurs
-const int MicroPhi = 3;       //on setup le pinout des microrupteurs
-const int MicroTheta = 4;     //on setup le pinout des microrupteurs
+const int BrasGaucheQuart = 12500;
+const int BrasGaucheDemi = 50000;
+const int BrasDroisQuart = 12500;
+const int BrasDroisDemi = 50000;
+const int TeteQuart = 12500;
+const int TeteDemi = 25000;
 
-const int stepsPerRevolution = 6400;         // on set le nombre de pas par revolution
-const int TetePhiInitial = 267;              //on fixe une valeur de position d'équilibre
-const int TeteThetaInitial = 1600;            //on fixe une valeur de position d'équilibre
-const int BrasDroitPositionInitiale = -534;  //on fixe une valeur de position d'équilibre
-const int BrasGauchePositionInitiale = 534;  //on fixe une valeur de position d'équilibre
-int TetePhiCourant = 0;
-int TeteThetaCourant = 0;
-int BrasDroitCourant = 0;
-int BrasGaucheCourant = 0;
-
-const int MaxTetePhi=267;
-const int MinTetePhi=-267;
-const int MaxTeteTheta=1600;
-const int MinTeteTheta=-1600;
-const int MaxBrasDroit=1700;
-const int MinBrasDroit=-1700;
-const int MaxBrasGauche=1700;
-const int MinBrasGauche=-1700;
-
-void drawAnimation(int16_t x, int16_t y, uint8_t bitmap[][8], int16_t w, int16_t h, uint16_t color, int16_t lengthbitmap){
-  MatriceDroite.clear();
-  MatriceGauche.clear();
-  for (int i = 0; i < lengthbitmap; i++){
-    MatriceDroite.drawBitmap(0, 0, bitmap[i], 8, 8, color);
-    MatriceGauche.drawBitmap(0, 0, bitmap[i], 8, 8, color);
-    MatriceDroite.writeDisplay();
-    MatriceGauche.writeDisplay();
-    delay(500);
-    MatriceDroite.clear();
-    MatriceGauche.clear();
-  }
+void InitTete()
+{ // Fonction permettant d'initaliser les moteurs et de les placer en position initiale
+  // Tant que le microrupteur n'est pas enclanché
+  do
+  {
+    TeteStepper.step(1); // On bouge le moteur vers sa position d'équilibre
+  } while (!MicroTete);
 }
 
-void drawAnimationDroite(int16_t x, int16_t y, uint8_t bitmap[][8], int16_t w, int16_t h, uint16_t color, int16_t lengthbitmap){
-  MatriceDroite.clear();
-  for (int i = 0; i < lengthbitmap; i++){
-    MatriceDroite.drawBitmap(0, 0, bitmap[i], 8, 8, color);
-    MatriceDroite.writeDisplay();
-    delay(500);
-    MatriceDroite.clear();
-  }
+void InitBras()
+{ // Fonction permettant d'initaliser les moteurs et de les placer en position initiale
+  // Tant que le microrupteur n'est pas enclanché
+  do
+  {
+    BrasDroitStepper.step(1); // On bouge le moteur vers sa position d'équilibre
+  } while (!MicroDroit);
+  do
+  {
+    BrasGaucheStepper.step(1); // On bouge le moteur vers sa position d'équilibre
+  } while (MicroGauche);
 }
 
-void drawAnimationGauche(int16_t x, int16_t y, uint8_t bitmap[][8], int16_t w, int16_t h, uint16_t color[], int16_t lengthbitmap){
-  MatriceGauche.clear();
-  for (int i = 0; i < lengthbitmap; i++){
-    MatriceGauche.drawBitmap(0, 0, bitmap[i], 8, 8, color[i]);
-    MatriceGauche.writeDisplay();
-    delay(500);
-    MatriceGauche.clear();
-  }
+void BrasGauche45()
+{
+  BrasGaucheStepper.step(BrasGaucheQuart); // On bouge le moteur vers sa position d'équilibre
 }
 
-
-void InitTete(){    //Fonction permettant d'initaliser les moteurs et de les placer en position initiale
-  do{
-    TeteThetaStepper.step(1);               //On décale légèrement le moteur
-  }while(digitalRead(MicroTheta) == LOW);   //Tant que le microrupteur n'est pas enclanché
-  do{
-    TetePhiStepper.step(1);                 //On décale légèrement le moteur
-  }while(digitalRead(MicroPhi) == LOW);     //Tant que le microrupteur n'est pas enclanché
-  TeteThetaStepper.step(TeteThetaInitial);  //On bouge le moteur vers sa position d'équilibre
-  TetePhiStepper.step(TetePhiInitial);      //On bouge le moteur vers sa position d'équilibre
+void BrasGauche90()
+{
+  BrasGaucheStepper.step(BrasGaucheDemi); // On bouge le moteur vers sa position d'équilibre
 }
 
-void InitBras(){    //Fonction permettant d'initaliser les moteurs et de les placer en position initiale
-  do{
-    BrasDroitStepper.step(-1);              //On décale légèrement le moteur
-  }while(digitalRead(MicroDroit) == LOW);   //Tant que le microrupteur n'est pas enclanché
-    do{
-    BrasGaucheStepper.step(1);              //On décale légèrement le moteur
-  }while(digitalRead(MicroGauche) == LOW);  //Tant que le microrupteur n'est pas enclanché
-  BrasDroitStepper.step(BrasDroitPositionInitiale);   //On bouge le moteur vers sa position d'équilibre
-  BrasGaucheStepper.step(BrasGauchePositionInitiale); //On bouge le moteur vers sa position d'équilibre
+void BrasDroit45()
+{
+  BrasDroitStepper.step(BrasDroisQuart); // On bouge le moteur vers sa position d'équilibre
 }
 
-void HochementTete(){
-  TetePhiStepper.step((-1*TetePhiInitial));
-  TetePhiStepper.step((2*TetePhiInitial));
-  TetePhiStepper.step((-1*TetePhiInitial));
+void BrasDroit90()
+{
+  BrasDroitStepper.step(BrasDroisDemi); // On bouge le moteur vers sa position d'équilibre
 }
 
-void NegationTete(){
-  TeteThetaStepper.step((-1*TeteThetaInitial));
-  TeteThetaStepper.step((2*TeteThetaInitial));
-  TeteThetaStepper.step((-1*TeteThetaInitial));
+void Tete45()
+{
+  TeteStepper.step(TeteQuart);
 }
-
-void BalancementBrasParrallele(){
-  BrasDroitStepper.step((-1*BrasDroitPositionInitiale));
-  BrasGaucheStepper.step((-1*BrasGauchePositionInitiale));
-  BrasDroitStepper.step((2*BrasDroitPositionInitiale));
-  BrasGaucheStepper.step((2*BrasGauchePositionInitiale));
-  BrasDroitStepper.step((-1*BrasDroitPositionInitiale));
-  BrasGaucheStepper.step((-1*BrasGauchePositionInitiale));
+void Tete90()
+{
+  TeteStepper.step(TeteDemi);
 }
-
-void BalancementBrasOppose(){
-  BrasDroitStepper.step((-1*BrasDroitPositionInitiale));
-  BrasGaucheStepper.step((BrasGauchePositionInitiale));
-  BrasDroitStepper.step((2*BrasDroitPositionInitiale));
-  BrasGaucheStepper.step((-2*BrasGauchePositionInitiale));
-  BrasDroitStepper.step((-1*BrasDroitPositionInitiale));
-  BrasGaucheStepper.step((BrasGauchePositionInitiale));
+void TeteCustom(int angle)
+{
+  int nbstep = angle * 278;
+  TeteStepper.step(nbstep);
 }
-
-void PhyCustom(int nbstep){
-  TetePhiCourant = TetePhiCourant+nbstep;
-  if(TetePhiCourant>MaxTetePhi){
-    nbstep=nbstep-(TetePhiCourant-MaxTetePhi);
-    TetePhiCourant=MaxTetePhi;
-  }
-  if(TetePhiCourant<MinTetePhi){
-    nbstep=nbstep-(TetePhiCourant-MinTetePhi);
-    TetePhiCourant=MinTetePhi;
-  }
-  TetePhiStepper.step(nbstep);
-}
-
-void ThetaCustom(int nbstep){
-    TeteThetaCourant = TeteThetaCourant+nbstep;
-  if(TeteThetaCourant>MaxTeteTheta){
-    nbstep=nbstep-(TeteThetaCourant-MaxTeteTheta);
-    TeteThetaCourant=MaxTeteTheta;
-  }
-  if(TeteThetaCourant<MinTeteTheta){
-    nbstep=nbstep-(TeteThetaCourant-MinTeteTheta);
-    TeteThetaCourant=MinTeteTheta;
-  }
-  TeteThetaStepper.step(nbstep);
-}
-
-void DroitCustom(int nbstep){
-    BrasDroitCourant = BrasDroitCourant+nbstep;
-  if(BrasDroitCourant>MaxBrasDroit){
-    nbstep=nbstep-(BrasDroitCourant-MaxBrasDroit);
-    BrasDroitCourant=MaxBrasDroit;
-  }
-  if(BrasDroitCourant<MinBrasDroit){
-    nbstep=nbstep-(BrasDroitCourant-MinBrasDroit);
-    BrasDroitCourant=MinBrasDroit;
-  }
+void BrasDroitCustom(int angle)
+{
+  int nbstep = angle * 278;
   BrasDroitStepper.step(nbstep);
 }
-
-void GaucheCustom(int nbstep){
-    BrasGaucheCourant = BrasGaucheCourant+nbstep;
-  if(BrasGaucheCourant>MaxBrasGauche){
-    nbstep=nbstep-(BrasGaucheCourant-MaxBrasGauche);
-    BrasGaucheCourant=MaxBrasGauche;
-  }
-  if(BrasGaucheCourant<MinBrasGauche){
-    nbstep=nbstep-(BrasGaucheCourant-MinBrasGauche);
-    BrasGaucheCourant=MinBrasGauche;
-  }
+void BrasGaucheCustom(int angle)
+{
+  int nbstep = angle * 278;
   BrasGaucheStepper.step(nbstep);
 }
--- arduino/Robot_Accueil_ESIEA.ino
+++ arduino/Robot_Accueil_ESIEA.ino
@@ -1,78 +1,64 @@
 #include "Robot_Accueil_ESIEA.h"
-        
-int value=0;
-Adafruit_BicolorMatrix MatriceGauche;
-Adafruit_BicolorMatrix MatriceDroite;
-void setup() {
+
+int value = 0;
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("8x8 LED Matrix Test");
-  
-  MatriceGauche.begin(0x70);  // pass in the address
-  MatriceDroite.begin(0x71);  
-TetePhiStepper.setSpeed(30);     //La vitesse est en rotation par minute
-TeteThetaStepper.setSpeed(20);   //La vitesse est en rotation par minute
-BrasDroitStepper.setSpeed(30);   //La vitesse est en rotation par minute
-BrasGaucheStepper.setSpeed(30);  //La vitesse est en rotation par minute
 
+  TeteStepper.setSpeed(100);       // La vitesse est en rotation par minute
+  BrasDroitStepper.setSpeed(200);  // La vitesse est en rotation par minute
+  BrasGaucheStepper.setSpeed(500); // La vitesse est en rotation par minute
 }
 
-void loop() {
-    if(Serial.available()){
-        char tab[8];
-        String c = Serial.readString();
-        c.toCharArray(tab, 8);
-        Serial.print(c);
-        value=6400;
+void loop()
+{
+  if (Serial.available())
+  {
+    char tab[8];
+    String c = Serial.readString();
+    c.toCharArray(tab, 8);
+    Serial.print(c);
+    value = 16400;
 
-static const uint16_t color[8] = {
-  LED_GREEN,
-  LED_RED,
-  LED_YELLOW,
-  LED_GREEN,
-  LED_RED,
-  LED_YELLOW,
-  LED_GREEN,
-  LED_RED
-};
-        
-        /// DEBUT DU SWITCH CASE
-        switch (tab[0]) {
-          case 'B': //Bonjour
-            DroitCustom(value);
-            drawAnimationGauche(0, 0, AWAKE, 8, 8, color,AWAKE_LEN);
-            ThetaCustom(value);
-            Serial.print("\n");
-            break;
-          case 'A': //au revoir
-              DroitCustom(value);
-              drawAnimationGauche(0, 0, FERME, 8, 8, LED_GREEN,FERME_LEN);
-              ThetaCustom(-value);
-            Serial.print("\n");
-            break;
-          case 'T'://Trop proche
-              GaucheCustom(value);
-              drawAnimationGauche(0, 0, ENERVE, 8, 8, LED_RED,ENERVE_LEN);
-            Serial.print("\n");
-            break;
-          case 'L'://Longtemps
-             drawAnimationGauche(0, 0, HAPPY, 8, 8, LED_RED,HAPPY_LEN);
-            Serial.print("\n");
-            break;
-          case 'O'://OUI
-            HochementTete();
-            Serial.print("\n");
-            break;
-          case 'N'://NON
-            NegationTete();
-            break;
-          case 'M'://Marcher
-            BalancementBrasOppose();
-            break;    
-        default:
-          ThetaCustom(value);
-          Serial.print(c);
-          break;
-        }
+    /// DEBUT DU SWITCH CASE
+    switch (tab[0])
+    {
+    case 'A': // au revoir
+      //InitBras();
+      break;
+    case 'B':
+      //InitTete();
+      break;
+    case 'C':
+      BrasDroit45();
+      Serial.print("\n");
+      break;
+    case 'D':
+      BrasDroit90();
+      Serial.print("\n");
+      break;
+    case 'E':
+      BrasGauche45();
+      Serial.print("\n");
+      break;
+    case 'F':
+      BrasGauche90();
+      Serial.print("\n");
+      break;
+    case 'G':
+      Tete45();
+      Serial.print("\n");
+      break;
+    case 'H':
+      Tete90();
+      Serial.print("\n");
+      break;
+    default:
+      //BrasDroit45();
+      Serial.print(c);
+      break;
     }
+  }
 }
